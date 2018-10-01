9c9d3d4b56ee4d583b2c7d6b217eaa8095986aa6
xulihuazj <xulihuazj@foxmail.com>
2018-10-01 16:47:42 +0800

    添加授权认证及其他配置项目
    添加授权认证及其他配置项目

--- gateway-service/src/main/java/com/xulihuazj/gatewayservice/GatewayServiceApplication.java
+++ gateway-service/src/main/java/com/xulihuazj/gatewayservice/GatewayServiceApplication.java
@@ -2,8 +2,14 @@ package com.xulihuazj.gatewayservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
+// 开启网关
+@EnableZuulProxy
+// 注册服务
+@EnableEurekaClient
 public class GatewayServiceApplication {
 
     public static void main(String[] args) {
