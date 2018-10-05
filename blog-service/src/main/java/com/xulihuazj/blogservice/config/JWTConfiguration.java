e495fedd9c368ee0b62fb68826626b9febc7fec9
xulihuazj <xulihuazj@foxmail.com>
2018-10-05 14:49:43 +0800

    添加 JWT Token 验证配置
    添加 JWT Token 验证配置

--- blog-service/src/main/java/com/xulihuazj/blogservice/config/JWTConfiguration.java
+++ blog-service/src/main/java/com/xulihuazj/blogservice/config/JWTConfiguration.java
@@ -1,9 +1,43 @@
 package com.xulihuazj.blogservice.config;
 
+import jdk.management.resource.ResourceType;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
+import org.springframework.util.FileCopyUtils;
+
+import java.io.IOException;
 
 @Configuration
 public class JWTConfiguration {
 
-    
+    @Autowired
+    private JwtAccessTokenConverter jwtAccessTokenConverter;
+
+    @Bean
+    @Qualifier("tokenStore")
+    public TokenStore tokenStore() {
+        return new JwtTokenStore(jwtAccessTokenConverter);
+    }
+
+    @Bean
+    protected JwtAccessTokenConverter jwtToken() {
+        JwtAccessTokenConverter jwtAccessTokenConverter = new JwtAccessTokenConverter();
+        Resource resource = new ClassPathResource("public.cert");
+        String publicKey;
+        try {
+            publicKey = new String(FileCopyUtils.copyToByteArray(resource.getFile()));
+        } catch (IOException ex) {
+            throw new RuntimeException(ex);
+        }
+        jwtAccessTokenConverter.setVerifierKey(publicKey);
+        return jwtAccessTokenConverter;
+    }
+
 }
